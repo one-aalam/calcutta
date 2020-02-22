68479c7f58ad0220a6501216e21a5a6ada35586f
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-02-23 01:08:47 +0530

    Day: Enhnce style and conditionally dispatch events

--- src/components/Day.svelte
+++ src/components/Day.svelte
@@ -1,18 +1,27 @@
 <script>
-    import { getDate, isToday, isPast, isWeekend } from 'date-fns';
+    import { createEventDispatcher } from 'svelte';
+    import { getDate, isToday, isPast, isWeekend, isFuture } from 'date-fns';
 
-
+    const dispatch = createEventDispatcher();
     export let day;
     export let today = '';
 
     export let hint = false;
 
+    function onClick() {
+        if (!isPast(day, today)) {
+            dispatch('click');
+        }
+    }
+
     $: date = getDate(day);
 </script>
-<div on:click class="cal__date {isToday(day) ? 'cal__date--today font-bold rounded-full bg-indigo-100 text-indigo-600' : ''}
+<button on:click={onClick}
+            class="cal__date {isToday(day) ? 'cal__date--today font-bold bg-indigo-100 text-indigo-600' : ''}
             {isWeekend(day) && !isPast(day, today) && !hint ? 'cal__date-weekend text-indigo-600' : ''}
-            {isPast(day, today) ? 'cal__date-prev text-gray-500' : ''}
+            {isPast(day, today) ? 'cal__date-prev text-gray-500 cursor-not-allowed disabled' : ''}
+            {isToday(day, today) || isFuture(day, today) ? 'hover:bg-gray-200' : ''}
             {hint ? 'italic text-gray-300' : ''}
-            w-16 h-16 p-4 justify-center align-center">
+            w-16 h-16 p-4 rounded-full justify-center content-center self-center">
     {date}
-</div>
+</button>
